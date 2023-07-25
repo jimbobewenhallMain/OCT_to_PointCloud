9ee1cf9e1c273aa23bf25ea1aa504dcce1244dc1
jimbobewenhallMain <95866610+jimbobewenhallMain@users.noreply.github.com>
2023-07-25 15:39:11 +0100

    Update predict.py to actually work

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn.functional as F
 from PIL import Image
 
-from unet import UNet
+from segmentation_models_pytorch import UnetPlusPlus
 from utils.data_loading import BasicDataset
 from utils.utils import plot_img_and_mask
 
@@ -25,10 +25,9 @@ def predict_img(net,
     with torch.no_grad():
         output = net(img).cpu()
         output = F.interpolate(output, (full_img.size[1], full_img.size[0]), mode='bilinear')
-        if net.n_classes > 1:
-            mask = output.argmax(dim=1)
-        else:
-            mask = torch.sigmoid(output) > out_threshold
+
+        mask = output.argmax(dim=1)
+
 
     return mask[0].long().squeeze().numpy()
 
@@ -48,6 +47,7 @@ def get_args():
                         help='Scale factor for the input images')
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
     parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
+    parser.add_argument('--encoder', '-e', type=str, default='resnet152')
 
     return parser.parse_args()
 
@@ -56,7 +56,7 @@ def get_output_filenames(args):
     def _generate_name(fn):
         return f'{os.path.splitext(fn)[0]}_OUT.png'
 
-    return args.Unet_mask or list(map(_generate_name, args.input))
+    return args.output or list(map(_generate_name, args.input))
 
 
 def mask_to_image(mask: np.ndarray, mask_values):
@@ -71,7 +71,10 @@ def mask_to_image(mask: np.ndarray, mask_values):
         mask = np.argmax(mask, axis=0)
 
     for i, v in enumerate(mask_values):
-        out[mask == i] = v
+        if i == 0:
+            out[mask == i] = 0
+        else:
+            out[mask == i] = 255
 
     return Image.fromarray(out)
 
@@ -83,7 +86,7 @@ if __name__ == '__main__':
     in_files = args.input
     out_files = get_output_filenames(args)
 
-    net = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
+    net = UnetPlusPlus(in_channels=3, classes=2, encoder_name=args.encoder, encoder_weights=None)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Loading model {args.model}')
